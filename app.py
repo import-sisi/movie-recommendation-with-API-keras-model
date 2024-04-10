f90bd2b0f75b7459b8481c590e3fccd5e2b5d61a
import-sisi <ezertech210@gmail.com>
2024-04-10 19:14:37 +0330

    fix:rtl

--- app.py
+++ app.py
@@ -22,13 +22,9 @@ st.markdown("""
 <style>
 body, html {
     direction: RTL;
-    unicode-bidi: bidi-override;
-    text-align: right;
 }
 p, div, input, label, h1, h2, h3, h4, h5, h6 {
     direction: RTL;
-    unicode-bidi: bidi-override;
-    text-align: right;
 }
 </style>
 """, unsafe_allow_html=True)
